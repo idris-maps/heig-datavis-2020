f2a06af0dfddf10dcf460ed82d397bd0a0ebdedd
Anders <idris.maps@gmx.ch>
2020-02-27 19:20:21 +0100

    Ajouter code pour vérifier le résultat

--- 20200228/exercice_1.js
+++ 20200228/exercice_1.js
@@ -1,6 +1,6 @@
 const villes = [
   { nom: 'Lausanne', population: 138905 },
-  { nom: 'Yverdon-les-Bains', population: 30143 },
+  { nom: 'Yverdon', population: 30143 },
   { nom: 'Montreux', population: 26574 },
   { nom: 'Renens', population: 21036 },
   { nom: 'Nyon', population: 20533 },
@@ -50,3 +50,44 @@ const sommeHabitants = villes
 console.log('Nombre total d\'habitants', sommeHabitants)
 
 // 257018
+
+
+
+
+// Code pour vérifier le résultat
+
+const correct = {
+  "nomsDesVilles": [
+    "Lausanne",
+    "Yverdon",
+    "Montreux",
+    "Renens",
+    "Nyon",
+    "Vevey"
+  ],
+  "villesDePlusDe30000Habitants": [
+    {
+      "nom": "Lausanne",
+      "population": 138905
+    },
+    {
+      "nom": "Yverdon",
+      "population": 30143
+    }
+  ],
+  "habitantsYverdon": 30143,
+  "sommeHabitants": 257018
+}
+
+const compare = (a, b) =>
+  JSON.stringify(a) === JSON.stringify(b)
+
+console.log(`
+Réponses justes:
+
+1. Noms des villes: ${compare(nomsDesVilles, correct.nomsDesVilles)}
+2. Ville de plus de 30000 habitants: ${compare(villesDePlusDe30000Habitants, correct.villesDePlusDe30000Habitants)}
+3. Nombre d\'habitants à Yverdon: ${compare(habitantsYverdon, correct.habitantsYverdon)}
+4. Nombre total d\'habitants: ${compare(sommeHabitants, correct.sommeHabitants)}
+
+`)
