3d450f828d28dbb3f00f866930b4f4f139992f2f
Anders <idris.maps@gmx.ch>
2020-03-17 14:40:06 +0100

    Add regions

--- 20200320/rosling_data/toJSON_regions.js
+++ 20200320/rosling_data/toJSON_regions.js
@@ -1,18 +1,19 @@
 const d3 = require('d3')
 const fs = require('fs')
-const R = require('ramda')
 
 const csv = fs.readFileSync(`${__dirname}/temp/regions.csv`, 'utf-8')
 const json = d3.csvParse(csv)
 
+const getData = ({ geo, name, six_regions }) => ({
+  geo,
+  name,
+  region: six_regions,
+})
+
 console.log(
   JSON.stringify(
-    json.map(d => ({
-      geo: d.geo,
-      region: d.six_regions,
-      name: d.name,
-    })).filter(d => d.geo !== ''),
-    null,
-    2
+    json
+      .map(getData)
+      .filter(d => d.geo !== ''),
   )
 )
